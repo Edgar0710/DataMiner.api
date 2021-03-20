ccda8ef70299824ee0740b10e70b77a4ea7a1b00
Edgar0710 <60529884+Edgar0710@users.noreply.github.com>
2021-03-20 10:49:52 -0600

    Update DataRepository.cs

--- DataMiner.Data/DataRepository.cs
+++ DataMiner.Data/DataRepository.cs
@@ -10,7 +10,7 @@ namespace dataMiner.Data
         
         public DataRepository()
         {
-           string strConnection = @"Data Source=45.63.18.178\LETS;Initial Catalog=dACTMON; Persist Security Info=True;User ID=sa;Password=Lets_2020; Pooling=true; Max Pool Size=200; MultipleActiveResultSets=True;";
+           string strConnection = @"Data Source=45.63.18.178\LETS;Initial Catalog=dACTMON; Persist Security Info=True;User ID=actmon;Password=@ctmon_2021.; Pooling=true; Max Pool Size=200; MultipleActiveResultSets=True;";
           conn = new SqlConnection(strConnection);
         }
         public void Dispose()
